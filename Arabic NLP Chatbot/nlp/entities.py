143a2f4df8e4ebd3f890d3ab1fca82f1ddfaf8c8
Rida <101403393+ridarafiq09@users.noreply.github.com>
2025-12-18 21:02:11 +0400

    Add files via upload

--- Arabic NLP Chatbot/nlp/entities.py
+++ Arabic NLP Chatbot/nlp/entities.py
@@ -1,1 +1,16 @@
+def extract_entities(text):
+    entities = {}
 
+    products = ["فستان", "حذاء", "جاكيت"]
+    for p in products:
+        if p in text:
+            entities["product"] = p
+            break
+
+    if any(w in text for w in ["مقاسات", "المقاسات"]):
+        entities["sizes"] = True
+
+    if any(w in text for w in ["ألوان", "الألوان"]):
+        entities["colors"] = True
+
+    return entities
